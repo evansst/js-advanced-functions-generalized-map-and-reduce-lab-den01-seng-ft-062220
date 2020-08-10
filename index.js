3b894f118d869d6c5c0b55d9c017e0e8b6026ea9
Sam Evans <evansst@icloud.com>
2020-08-10 15:35:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your functions here
+function map(array, func, i = 0) {
+  array[i] = func(array[i]);
+  return array[i + 1] 
+    ? map(array, func, i + 1)
+    : array;
+}
+
+function reduce(array, func, startingValue) {
+  array[0] = (startingValue !== undefined) ? func(array[0], startingValue) : array[0];
+  return array[1] !== undefined 
+    ? reduce(array.slice(1), func, array[0])
+    : array[0];
+}
